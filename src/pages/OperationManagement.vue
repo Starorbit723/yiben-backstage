6aeef740e607b9bc5dfbbfd65e224993a33ac23b
sca <happysca@gmail.com>
2025-05-03 16:09:32 +0800

    文件上传

--- src/pages/OperationManagement.vue
+++ src/pages/OperationManagement.vue
@@ -1,44 +1,84 @@
 <template>
   <div class="operationmanagement-page">
     <el-upload
-      class="avatar-uploader"
-      action="https://jsonplaceholder.typicode.com/posts/"
-      :show-file-list="false"
-      :on-success="handleAvatarSuccess"
-      :before-upload="beforeAvatarUpload">
-      <img v-if="imageUrl" :src="imageUrl" class="avatar">
-      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+      class="upload-demo"
+      ref="upload"
+      action=""
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :on-change="handleChange"
+      :file-list="fileList"
+      :auto-upload="false">
+      <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+      <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
     </el-upload>
+    <div v-if="previewUrl" class="preview-container">
+      <img :src="previewUrl" class="preview-image" />
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'OperationManagement',
   data() {
     return {
-      imageUrl: ''
+      fileList: [],
+      previewUrl: '',
+      currentFile: null
     }
   },
   mounted() {
     
   },
-  methods:{
-    handleAvatarSuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
-    },
-    beforeAvatarUpload(file) {
-      // const isJPG = file.type === 'image/jpeg';
-      const isLt2M = file.size / 1024 / 1024 < 2;
+  methods: {
+    submitUpload() {
+      if (this.currentFile) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const binaryData = e.target.result;
+          console.log('文件二进制数据:', binaryData);
+        };
+        reader.readAsArrayBuffer(this.currentFile);
 
-      // if (!isJPG) {
-      //   this.$message.error('上传头像图片只能是 JPG 格式!');
-      // }
-      if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!');
+        // 使用文件原始名称作为云存储路径
+        const cloudPath = `uploads/${Date.now()}-${this.currentFile.name}`;
+        
+        this.$cloudbase.uploadFile({
+          cloudPath: cloudPath,
+          filePath: this.currentFile,
+          onUploadProgress: (progressEvent) => {
+            console.log('上传进度:', progressEvent);
+          }
+        })
+        .then(res => {
+          console.log('上传成功:', res);
+          /* {
+              "fileID": "cloud://cloud1-0gvvdaq4c40b8f74.636c-cloud1-0gvvdaq4c40b8f74-1351667792/uploads/1746257971848-layer-example9.a574f6d1.png",
+              "download_url": "https://636c-cloud1-0gvvdaq4c40b8f74-1351667792.tcb.qcloud.la/uploads/1746257971848-layer-example9.a574f6d1.png?sign=b23c83f2eef404a90c2d1b6536d0cc1f&t=1746257974",
+              "requestId": "66874cfcc5ead"
+          } */
+          this.$message.success('文件上传成功');
+        })
+        .catch(err => {
+          console.error('上传失败:', err);
+          this.$message.error('文件上传失败');
+        });
       }
-      return isLt2M;
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+      this.previewUrl = '';
+      this.currentFile = null;
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleChange(file) {
+      this.currentFile = file.raw;
+      // 创建预览URL
+      this.previewUrl = URL.createObjectURL(file.raw);
     }
   }
 }
@@ -68,6 +108,14 @@ export default {
     width: 178px;
     height: 178px;
     display: block;
+  }
+  .preview-container {
+    margin-top: 20px;
+    .preview-image {
+      max-width: 100%;
+      max-height: 300px;
+      object-fit: contain;
+    }
   }
 }
 </style>
